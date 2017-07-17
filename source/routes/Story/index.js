9d61e321d08239f1eec2cd1bf8d6c58a2b20671b
Mark Hansen <x@markthemark.com>
2017-07-17 11:49:00 -0400

    ...

--- source/routes/Story/index.js
+++ source/routes/Story/index.js
@@ -14,6 +14,14 @@ export default class StoryPage extends Component {
 		super(props);
 		this.store = this.props.store;
 	}
+	componentDidMount() {
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-102643571-1', 'auto');
+		ga('send', 'pageview');
+	}
 	render() {
 		const story = this.store.storyState.story;
 		const scraped = this.store.storyState.story.scraped;
