0eddb6a3de44400bc01cb8795a9638cb8f6aed95
Mark Hansen <x@markthemark.com>
2017-07-17 01:34:55 -0400

    ...

--- source/component-library/Navigation/EdFooter.js
+++ source/component-library/Navigation/EdFooter.js
@@ -31,13 +31,13 @@ class EdFooter extends Component {
         {!this.state.read && <div className="footer__finish-prompt">
           <p>Finish the article?</p><button onClick={() => this.setState({ ...this.state, read: true })}>Finished!</button>
         </div>}
-        {(this.state.read && !this.state.change) && <div className="footer__initial-thoughts">
+        {(this.state.read && this.state.change == null) && <div className="footer__initial-thoughts">
           <p>Has this article changed your views?</p>
           <button onClick={() => this.setState({ ...this.state, revealed: true, change: 0 })}>No</button>
           <button onClick={() => this.setState({ ...this.state, revealed: true, change: 1 })}>Somewhat</button>
           <button onClick={() => this.setState({ ...this.state, revealed: true, change: 2 })}>Yes</button>
         </div>}
-        {(this.state.read && this.state.change) && <div className="footer__conclusion">
+        {(this.state.read && this.state.change != null) && <div className="footer__conclusion">
           <h5>This is an article from the New York Times</h5>
           <a href={`https://nytimes.com${story.slug}`} target="_blank">{`https://nytimes.com${story.slug}`}</a>
           <p>I'm concerned that myself and others do not hear those they disagree with. I made two parody sites, one that presents Fox News content to NY Times readers and vice versa, with the hope we will judge arguments and information by their own strength.</p>
